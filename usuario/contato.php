4212ef77ddde289d8a0bf384e739088bb07b8904
IOHANA ALMEIDA RODRIGUES <0258-067805-3@online.sescpr.com.br>
2024-09-30 11:02:31 -0300

    versão numero 3

--- usuario/contato.php
+++ usuario/contato.php
@@ -3,48 +3,56 @@
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <title>Nós amamos animais </title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <title>Mundopet.com</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="index.css" type="stylesheet"></script>
+    <title><Mundopet class="com"></Mundopet></title>
   </head>
+
+  <style>
+    @import url('https://fonts.cdnfonts.com/css/copperplate');
+  </style>
+
   <body>
+    <link href="https://fonts.cdnfonts.com/css/copperplate" rel="stylesheet">
 
-  <nav class="navbar navbar-expand-lg bg-danger text-white" data-bs-theme="dark">
-    <div class="container-fluid">
-     <i class="bi bi-heart-fill"></i>
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
-<path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
-</svg> <br>
-      <a class="navbar-brand  text-white" href="#">Nós amamos os animais </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon  text-white "></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarColor02">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link  text-white" href="./index.html">Início</a>
-          </li>
-
-          <li class="nav-item">
-            <a class="nav-link active  text-white" aria-current="page" href="./castração.html">Castração</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link  text-white" href="./contato.html"> Contato </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link  text-white" href="./animais.html">Animais</a>
-          </li>
-        </ul>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-light" type="submit">Search</button>
-        </form>
+    <nav class="navbar navbar-expand-lg text-white" style="background-color:#ef42c4; overflow: hidden; position: fixed; top: 0; width: 100%; z-index: 100;">
+      <div class="container-fluid">
+       <i class="bi bi-heart-fill"></i>
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+    <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+  </svg><br>
+        <a class="navbar-brand  text-white" href="#">Mundo Pet </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon  text-white "></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarColor02">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link  text-white" href="./index.html">Início</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active  text-white" aria-current="page" href="./castração.html">Castração</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link  text-white" href="./contato.php"> Contato </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link  text-white" href="./animais.html">Animais</a>
+            </li>
+          </ul>
+          <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-light" type="submit">Search</button>
+          </form>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
 <center> <h1>Cadastro do Adotante </h1>
   <div class= "container">
   <form class="form- signin" id="cadastro" onevent="tratarFormulario()">
